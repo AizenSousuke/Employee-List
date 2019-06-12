f12467b2c4d7ae18072e9975a69ac950f0cfdeb1
AizenSousuke <puteranikdimas@yahoo.com.sg>
2019-06-12 17:03:41 +0800

    Added router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'employeelist', component: EmployeeListComponent },
+  { path: 'employeedetail', component: EmployeeDetailComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
